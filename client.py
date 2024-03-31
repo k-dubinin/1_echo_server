39097fb771f8f90d7a50a6da0c4a85e3beac6fd5
k-dubinin <81645875+k-dubinin@users.noreply.github.com>
2024-03-31 17:12:48 +0300

    Клиент для эхо сервера(4 задание)

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 import socket
 from time import sleep
-
+HOST=input('Введите имя хоста или айпи адрес: ')
+PORT=int(input('Введите порт сервера: '))
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect((HOST, PORT))
 
-#msg = input()
-msg = "Hi!"
+msg = input('Введите ваше сообщение: ')
+#msg = "Hi!"
 sock.send(msg.encode())
 
 data = sock.recv(1024)
-
-sock.close()
-
 print(data.decode())
+sock.close()
